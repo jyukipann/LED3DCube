35f040163170ecdf2ecc4cc3133160e4443eadf7
JUKI <jyukipann@yahoo.co.jp>
2021-04-27 10:10:00 +0900

    iikanji

--- game/LEDCubeClass.py
+++ game/LEDCubeClass.py
@@ -1,6 +1,41 @@
 import numpy as np
 import threading as thread
 
+class Color:
+	def __init__(self,R,G,B):
+		self.setColor(R,G,B)
+
+	def setColor(self,R,G,B):
+		#int 0 <= R,G,B, <= 255
+		self.R = R
+		self.G = G
+		self.B = B
+	
+	def __list__(self):
+		return [self.R,self.G,self.B]
+	
+	def getColor(self):
+		return np.ndarray(list(self))
+
+class LED_Cube:
+	def __init__(self,n=5):
+		self.n = 5
+		self.cube = np.zeros((self.n,self.n,self.n,3))
+
+	def __reset__(self):
+		self.cube = np.zeros((self.n,self.n,self.n,3))
+	
+	def set_color(self,x,y,z,color):
+		#int x,y,z >= 0 and color isInstans(Color)
+		self.cube[x,y,z] = color.getColor()
+
+	def output(self):
+		pass
+
+	def __str__(self):
+		return str(self.cube)
+
+
 class gobang:
 	def __init__(self,n=5):
 		self.n = n
