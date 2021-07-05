945338eea768ca9052ce4863a9ddf7e7c3c89293
JUKI <jyukipann@yahoo.co.jp>
2021-07-05 15:44:32 +0900

    color

--- test/LEDCheck.py
+++ test/LEDCheck.py
@@ -167,7 +167,7 @@ def flying_bee(strip,color,wait_ms=60):
 	pos = [3,3,3]
 	mat5 = np.zeros((5,5,5,3),dtype=int)
 	print(*mat5[pos[0],pos[1],pos[2]])
-	strip.setPixelColor(m2s[pos[0],pos[1],pos[2]],Color(*mat5[pos[0],pos[1],pos[2]]))
+	strip.setPixelColor(m2s[pos[0],pos[1],pos[2]],color)
 	strip.show()
 	time.sleep(wait_ms/1000.0)
 	for i in range(100):
@@ -181,7 +181,7 @@ def flying_bee(strip,color,wait_ms=60):
 			if(nextPos[j] >= 5):
 				nextPos[j] = 4
 		pos = nextPos
-		strip.setPixelColor(m2s[pos[0],pos[1],pos[2]],Color(*mat5[pos[0],pos[1],pos[2]]))
+		strip.setPixelColor(m2s[pos[0],pos[1],pos[2]],color)
 		strip.show()
 		time.sleep(wait_ms/1000.0)
 		print(*pos)
